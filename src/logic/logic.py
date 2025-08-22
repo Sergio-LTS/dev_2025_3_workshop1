43f5d5597e82dad5efe071764a9a17d8159b3845
Sergio-LTS <sdzuniga64@ucatolica.edu.co>
2025-08-22 14:22:52 -0500

    main

--- src/logic/logic.py
+++ src/logic/logic.py
@@ -10,29 +10,10 @@ class Logica:
         return a or b
     
     def NOT(self, a):
-        """
-        Implementa la operación lógica NOT.
-        
-        Args:
-            a (bool): Valor booleano
-            
-        Returns:
-            bool: Resultado de NOT a
-        """
-        pass
+        return not a
     
     def XOR(self, a, b):
-        """
-        Implementa la operación lógica XOR (OR exclusivo).
-        
-        Args:
-            a (bool): Primer valor booleano
-            b (bool): Segundo valor booleano
-            
-        Returns:
-            bool: Resultado de a XOR b
-        """
-        pass
+        return a != b
     
     def NAND(self, a, b):
         """
