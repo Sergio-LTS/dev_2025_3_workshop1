6a688e17a9fcccd9aff9867b343c9791ec340e3d
Sergio-LTS <sdzuniga64@ucatolica.edu.co>
2025-08-22 15:51:16 -0500

    log4

--- src/logic/logic.py
+++ src/logic/logic.py
@@ -28,16 +28,6 @@ class Logica:
         return not a or b
     
     def bi_implicacion(self, a, b):
-        """
-        Implementa la operación lógica de bi-implicación (a <-> b).
-        
-        Args:
-            a (bool): Primer valor booleano
-            b (bool): Segundo valor booleano
-            
-        Returns:
-            bool: Resultado de la bi-implicación
-        """
-        pass
+        return a == b
     
     
