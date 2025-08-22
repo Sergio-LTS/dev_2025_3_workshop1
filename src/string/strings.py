4cc627b091f1ea6d96f4af734f11ed131db01025
Sergio-LTS <sdzuniga64@ucatolica.edu.co>
2025-08-22 16:32:51 -0500

    str3

--- src/string/strings.py
+++ src/string/strings.py
@@ -30,16 +30,7 @@ class Strings:
         return " ".join(p.capitalize() for p in texto.split())
     
     def eliminar_espacios_duplicados(self, texto):
-        """
-        Elimina espacios duplicados en una cadena.
-        
-        Args:
-            texto (str): Cadena con posibles espacios duplicados
-            
-        Returns:
-            str: Cadena sin espacios duplicados
-        """
-        pass
+        return " ".join(texto.split())
     
     def es_numero_entero(self, texto):
         """
