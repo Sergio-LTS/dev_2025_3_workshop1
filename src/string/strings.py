6b8271f8e6930bf9c290a370415d9850f50185ce
Sergio-LTS <sdzuniga64@ucatolica.edu.co>
2025-08-23 20:29:01 -0500

    str7

--- src/string/strings.py
+++ src/string/strings.py
@@ -21,7 +21,7 @@ class Strings:
         return sum(1 for c in texto.lower() if c.isalpha() and c not in "aeiou")
     
     def es_anagrama(self, texto1, texto2):
-        return sorted(texto1.replace(" ", "").lower()) == sorted(texto2.replace(" ", "").lower()
+        return sorted(texto1.replace(" ", "").lower()) == sorted(texto2.replace(" ", "").lower())
     
     def contar_palabras(self, texto):
        return len(texto.split())
